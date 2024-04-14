ae34cdde1ddf86c3508a43af0ec8b15f5c34cedf
b22robhe <96245488+b22robhe@users.noreply.github.com>
2024-04-15 00:20:19 +0200

    Button functionality Made the Yes button change appText to "Good User" Made the No button change appText to "Bad User"

--- app/src/main/java/com/example/widgets/MainActivity.java
+++ app/src/main/java/com/example/widgets/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.widgets;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button yesBtn = findViewById(R.id.yesBtn);
+        final Button noBtn = findViewById(R.id.noBtn);
+        final TextView appText = findViewById(R.id.appText);
+        yesBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                appText.setText("Good User");
+            }
+        });
+
+
+        noBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                appText.setText("Bad User");
+            }
+        });
+
     }
 }
